6cc16d922d73359ff7a8c56f4bbfe1de4e510679
Nicorasuu <nicolaswo96@gmail.com>
2022-11-30 11:39:44 -0300

    30/11 manha

--- js/principal.js
+++ js/principal.js
@@ -1,18 +1,82 @@
 let titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var i = 0; i < pacientes.length; i++){
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    let paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var imc = peso / (altura * altura);
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-console.log(paciente);
-console.log(peso);
-console.log(altura);
-console.log(imc); 
+    var tdImc = paciente.querySelector(".info-imc");
 
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso < 0 || peso > 1000){
+        console.log ("Peso invalido");
+        pesoEhValido = false;
+        tdPeso.textContent = "Peso Invalido";
+        paciente.classList.add("paciente-invalido"); 
+    }
+
+    if (altura < 0 || altura > 4.00){
+        console.log("Altura invalida");
+        alturaEhValida = false;
+        tdAltura.textContent = "Altura invalida!";
+        paciente.classList.add("paciente-invalido"); 
+    }
+
+    if (alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    } else{
+        tdImc.textContent = "Altura e/ou peso invalidos!"
+    }
+
+    console.log(paciente);
+    console.log(peso);
+    console.log(altura);
+    console.log(imc); 
+    console.log(pacientes[i]);
+}
+
+let botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value
+    var gordura = form.gordura.value
+
+    var pacienteTr = document.createElement("tr"); 
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild (pacienteTr);
+})
+
+titulo.addEventListener("click", function(){
+    console.log("Ola eu fui clicado!");
+});
